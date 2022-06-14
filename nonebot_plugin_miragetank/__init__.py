9b901780355de9e1db62a7b30ebfd79edba5b1b7
Laffey <66518048+RafuiiChan@users.noreply.github.com>
2022-06-14 21:04:24 +0800

    修复参数获取的bug

--- nonebot_plugin_miragetank/__init__.py
+++ nonebot_plugin_miragetank/__init__.py
@@ -8,55 +8,59 @@ from nonebot.typing import T_State
 
 from .data_source import color_car, get_img, gray_car
 
-mirage_tank = on_command("幻影坦克", aliases={"miragetank"}, priority=27)
+mirage_tank = on_command(
+    "幻影坦克", aliases={"miragetank"}, priority=27
+)
 
 
 @mirage_tank.handle()
 async def handle_first(
-    bot: Bot, event: MessageEvent, state: T_State = State(), args: Message = CommandArg()
+    bot: Bot,
+    event: MessageEvent,
+    state: T_State = State(),
+    args: Message = CommandArg(),
 ):
     images = []
     for seg in args:
+        print(seg)
         if seg.type == "text":
             state["mod"] = seg.data["text"].strip()
         elif seg.type == "image":
             images.append(seg)
     if len(images) >= 1:
-        state['img1'] = images[0]
+        state["img1"] = images[0]
     if len(images) >= 2:
-        state['img2'] = images[1]
+        state["img2"] = images[1]
 
 
-@mirage_tank.got("mod", prompt="需要指定结果类型: gray | color")
-async def get_mod(
-    args: Message = CommandArg(),
-    state: T_State = State(),
-    cancel=HandleCancellation("已取消"),
-):
-    mod = args.extract_plain_text().strip()
-    if mod in ["gray", "color"]:
-        state["mod"] = mod
-    else:
+@mirage_tank.got(
+    "mod", prompt="需要指定结果类型: gray | color", parameterless=[HandleCancellation("已取消")]
+)
+async def get_mod(event: MessageEvent, state: T_State = State()):
+    mod = str(state["mod"]).strip()
+    if mod not in ["gray", "color"]:
         await mirage_tank.reject('"gray" | "color", 二选一')
 
 
-@mirage_tank.got("img1", prompt="请发送两张图，按表里顺序")
-@mirage_tank.got("img2", prompt="还需要一张图")
-async def get_images(
-    state: T_State = State(), cancel=HandleCancellation("已取消")
-):
-    img_urls = []
+@mirage_tank.got(
+    "img1", prompt="请发送两张图，按表里顺序", parameterless=[HandleCancellation("已取消")]
+)
+@mirage_tank.got("img2", prompt="还需要一张图", parameterless=[HandleCancellation("已取消")])
+async def get_images(state: T_State = State()):
+    imgs = []
     mod = str(state["mod"])
     for seg in state["img1"] + state["img2"]:
-        img_urls.append(await get_img(seg.data['url']))
+        if seg.type != "image":
+            await mirage_tank.reject("有个不是图")
+        imgs.append(await get_img(seg.data["url"]))
 
     await mirage_tank.send("开始合成")
 
     res = None
     if mod == "gray":
-        res = await gray_car(img_urls[0], img_urls[1])
+        res = await gray_car(imgs[0], imgs[1])
     elif mod == "color":
-        res = await color_car(img_urls[0], img_urls[1])
+        res = await color_car(imgs[0], imgs[1])
     if res:
         img_urls = []
         await mirage_tank.finish(
